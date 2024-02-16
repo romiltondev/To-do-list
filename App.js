d97f03537d24b40731cd0de44fea3abee7b4cfa1
romiltondev <156550707+romiltondev@users.noreply.github.com>
2024-02-16 15:46:53 -0300

    todo list
    Desenvolvimento de app de listagem de tarefas

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 
-export default function App() {
+const TodoApp = () => {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+
+  const handleAddTask = () => {
+    if (task !== '') {
+      setTaskList([...taskList, task]);
+      setTask('');
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const newList = [...taskList];
+    newList.splice(index, 1);
+    setTaskList(newList);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <StatusBar style="auto" />
+      <Text style={styles.title}>Lista de Tarefas</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Adicione uma tarefa"
+          value={task}
+          onChangeText={setTask}
+        />
+        <TouchableOpacity style={styles.addButton} onPress={handleAddTask}>
+          <Text style={styles.addButtonText}>Adicionar</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        style={styles.list}
+        data={taskList}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity onPress={() => handleDeleteTask(index)}>
+            <View style={styles.listItem}>
+              <Text>{item}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    color:'#008000',
+    fontWeight: 'bold',
+    marginTop:20,
+    marginBottom: 20,
+    textAlign:'center'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 30,
+  },
+  input: {
+    width: '70%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  addButton: {
+    backgroundColor: '#327355',
+    borderRadius: 5,
+    padding: 10,
+  },
+  addButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  list: {
+    flex: 1,
+  },
+  listItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    alignItems:'center'
   },
 });
+
+export default TodoApp;
+
